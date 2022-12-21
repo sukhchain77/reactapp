3dbd71f4b0c19835491cb4c7009f188880f9af70
sukhchain77 <sukhchaink317@gmail.com>
2022-12-21 20:07:34 +0530

    kk

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+  {/* <HashRouter> */}
     <App />
-  </React.StrictMode>,
+
+  {/* </HashRouter> */}
+ </React.StrictMode>
+  ,
   document.getElementById("root")
 );
 
